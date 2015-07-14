307a15f1f7039fe9140b31344c82b89a361d88ef
Jeremy Brooks <jeremybrooks321@gmail.com>
2015-07-14 20:25:25 +0100

    Started creating a basic 'code spike style' game module and tests

---
+++ src/game.js
@@ -0,0 +1,69 @@
+var board = null;
+
+// player
+function Player() {
+    this.row = 0;
+    this.col = 0;
+    this.lives = 2;
+}
+
+exports.reset = function(){
+    board = [[],[],[],[],[],[],[],[]];
+    //set up board
+    var rowCount = 0;
+    for (var colIndex = 0, col = null; col = board[colIndex]; colIndex++){
+        while (rowCount < 8){
+            col[rowCount] = "[ ]";
+            rowCount++;
+        }
+        rowCount = 0;
+    }
+    player = new Player();
+    // place player by default
+    board[player.col][player.row] = player;
+}
+
+exports.getBoardHeight = function () {
+    //assume this is a square s return length of first row as all are equal
+    return board[0].length;
+}
+
+exports.getBoardWidth = function(){
+    return board.length;
+}
+
+exports.getPlayer = function(){
+    return player;
+}
+
+exports.getPlayerPosition = function () {
+    var playerIndex = NaN;
+    var position = {
+        row: NaN,
+        col: NaN
+    }
+    for (var colIndex = 0, col; col = board[colIndex]; colIndex++){
+        playerIndex = col.indexOf(player);
+        if (playerIndex !== -1){
+            position.row = playerIndex;
+            position.col = colIndex;
+        }
+    }
+    return position;
+}
+
+exports.displayBoard = function () {
+    console.log(board);
+}
+
+exports.moveUp = function (){
+    board[player.col][player.row] = "[-]";
+    player.row += 1;
+    board[player.col][player.row] = player;
+};
+
+exports.moveRight = function(){
+    board[player.col][player.row] = "[-]";
+    player.col += 1;
+    board[player.col][player.row] = player;
+}
--- test/testRunner.js
+++ test/testRunner.js
@@ -1,4 +1,57 @@
 var assert = require('assert');
+var game = require('../src/game.js');
 
-assert.equal(true, true);
+var expectedRow = NaN;
+var expectedCol = NaN;
+
+function setUp(){
+    game.reset();
+    expectedCol = 0;
+    expectedRow = 0;
+}
+function tearDown(){
+    expectedCol = NaN;
+    expectedRow = NaN;
+}
+
+// wrap whole lot in try catch to better get error detail
+try {
+    setUp()
+    game.displayBoard();
+    assert.equal(game.getBoardWidth(), 8);
+    assert.equal(game.getBoardHeight(), 8);
+    tearDown();
+
+    // player
+    setUp();
+    assert.equal(game.getPlayer().row, expectedRow, "Player is in row 0 by default");
+    assert.equal(game.getPlayer().col, expectedCol, "Player is in col 0 by default");
+    assert.equal(game.getPlayer().lives, 2, "Player has 2 lives by default");
+    tearDown();
+
+    // position
+    setUp();
+    assert.equal(game.getPlayerPosition().row, expectedRow, "Player should be in row 0 by default");
+    assert.equal(game.getPlayerPosition().col, expectedCol, "Player should be in col 0 by default");
+    tearDown();
+
+    // moving up
+    setUp();
+    game.moveUp();
+    assert.equal(game.getPlayerPosition().row, expectedRow+1, "Moving player up increments their row by 1");
+    game.displayBoard();
+    tearDown();
+
+    // moving right
+    setUp();
+    game.moveRight();
+    assert.equal(game.getPlayerPosition().col, expectedCol+1, "Moving player right increments thier col by 1");
+    game.displayBoard();
+    tearDown();
+
+} catch (e) {
+    console.log(e);
+    // throw again so build can fail on this
+    throw e;
+}
 
