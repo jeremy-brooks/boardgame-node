6db8f1f93a9acf4ceb9bf97dff208d5126834e45
Jeremy Brooks <jeremybrooks321@gmail.com>
2015-07-15 22:00:12 +0100

    Made a square Class to better control the contents of each board position

--- src/game.js
+++ src/game.js
@@ -1,10 +1,24 @@
 var board = null;
+var player = null;
 
 // player
 function Player() {
     this.row = 0;
     this.col = 0;
     this.lives = 2;
+    this.graphic = "[P]";
+}
+
+// square
+function Square(hasMine, player) {
+    this.player = player || null;
+    this.hasMine = hasMine || false;
+    this.hasHit = false;
+    if (player) {
+        this.graphic = player.graphic;
+    } else {
+        this.graphic = (hasMine) ? "[X]" : "[ ]";
+    }
 }
 
 exports.reset = function(){
@@ -12,21 +26,22 @@ exports.reset = function(){
     //set up board
     var rowCount = 0;
     var hasMine = false;
+    var square = null;
+    player = new Player();
     for (var colIndex = 0, col = null; col = board[colIndex]; colIndex++){
         while (rowCount < 8){
             hasMine = !!Math.round(Math.random());
-            if (hasMine){
-                col[rowCount] = "[X]";
+            // if on the first square then put player there and override hasMine value
+            if (rowCount === 0 && colIndex === 0){
+                square = new Square(false, player);
             } else {
-                col[rowCount] = "[ ]";
+                square = new Square(hasMine);
             }
+            col[rowCount] = square;
             rowCount++;
         }
         rowCount = 0;
     }
-    player = new Player();
-    // place player by default
-    board[player.col][player.row] = player;
 }
 
 exports.getBoardHeight = function () {
@@ -40,22 +55,6 @@ exports.getBoardWidth = function(){
 
 exports.getPlayer = function(){
     return player;
-}
-
-exports.getPlayerPosition = function () {
-    var playerIndex = NaN;
-    var position = {
-        row: NaN,
-        col: NaN
-    }
-    for (var colIndex = 0, col; col = board[colIndex]; colIndex++){
-        playerIndex = col.indexOf(player);
-        if (playerIndex !== -1){
-            position.row = playerIndex;
-            position.col = colIndex;
-        }
-    }
-    return position;
 }
 
 exports.displayBoard = function () {
@@ -90,7 +89,7 @@ exports.getActiveMines = function () {
     var activeMineCount = 0;
     for (var ci = 0, col; col = board[ci]; ci++){
         for (var ri = 0, row; row = col[ri]; ri++){
-            if (row === "[X]") activeMineCount += 1;
+            if (row.hasMine) activeMineCount += 1;
         }
     }
     return activeMineCount;
@@ -104,4 +103,9 @@ exports.getStatus = function () {
     };
     console.log(status);
     return status;
+};
+
+exports.getSquareAt = function (coordinates) {
+    var square = board[coordinates.col][coordinates.row];
+    return square;
 };
--- test/testRunner.js
+++ test/testRunner.js
@@ -3,6 +3,8 @@ var game = require('../src/game.js');
 
 var expectedRow = NaN;
 var expectedCol = NaN;
+var square = null;
+var gameStatus = null;
 
 function setUp() {
     game.reset();
@@ -12,73 +14,75 @@ function setUp() {
 function tearDown() {
     expectedCol = NaN;
     expectedRow = NaN;
+    square = null;
+    gameStatus = null;
 }
 
-
-setUp()
-game.displayBoard();
+console.log("test the board dimensions");
+setUp();
 assert.equal(game.getBoardWidth(), 8);
 assert.equal(game.getBoardHeight(), 8);
 tearDown();
 
-// player
+console.log("test the player");
 setUp();
 assert.equal(game.getPlayer().row, expectedRow, "Player is in row 0 by default");
 assert.equal(game.getPlayer().col, expectedCol, "Player is in col 0 by default");
 assert.equal(game.getPlayer().lives, 2, "Player has 2 lives by default");
 tearDown();
 
-// position
-setUp();
-assert.equal(game.getPlayerPosition().row, expectedRow, "Player should be in row 0 by default");
-assert.equal(game.getPlayerPosition().col, expectedCol, "Player should be in col 0 by default");
-tearDown();
-
-// moving up
+console.log("test moving up");
 setUp();
 game.moveUp();
-assert.equal(game.getPlayerPosition().row, expectedRow + 1, "Moving player up increments their row by 1");
-game.displayBoard();
+assert.equal(game.getPlayer().row, expectedRow + 1, "Moving player up increments their row by 1");
 tearDown();
 
-// moving right
+console.log("test moving right");
 setUp();
 game.moveRight();
-assert.equal(game.getPlayerPosition().col, expectedCol + 1, "Moving player right increments thier col by 1");
-game.displayBoard();
+assert.equal(game.getPlayer().col, expectedCol + 1, "Moving player right increments thier col by 1");
 tearDown();
 
-// moving left
+console.log("test moving left");
 setUp();
 game.moveRight();
-assert.equal(game.getPlayerPosition().col, expectedCol + 1, "Moving player right increments thier col by 1");
+assert.equal(game.getPlayer().col, expectedCol + 1, "Moving player right increments thier col by 1");
 game.moveLeft();
-assert.equal(game.getPlayerPosition().col, expectedCol, "Moving the player left decrements thier col by 1");
-game.displayBoard();
+assert.equal(game.getPlayer().col, expectedCol, "Moving the player left decrements thier col by 1");
 tearDown();
 
-// moving down
+console.log("test moving up");
 setUp();
 game.moveUp();
-assert.equal(game.getPlayerPosition().row, expectedRow + 1, "Moving player up increments their row by 1");
+assert.equal(game.getPlayer().row, expectedRow + 1, "Moving player up increments their row by 1");
 game.moveDown();
-assert.equal(game.getPlayerPosition().row, expectedRow, "Moving player up decrements their row by 1");
-game.displayBoard();
+assert.equal(game.getPlayer().row, expectedRow, "Moving player up decrements their row by 1");
 tearDown();
 
-// land mines
+console.log("test land mines at start of game");
 setUp();
 assert.equal(game.getActiveMines() > 0, true, "Game board contains some mines by default");
 tearDown();
 
-// game status
+console.log("test game status at start of game");
 setUp();
-var gameStatus = game.getStatus();
+gameStatus = game.getStatus();
 assert.equal(gameStatus.player.col, 0, "Player is in first column");
 assert.equal(gameStatus.player.row, 0, "Player is in first row");
 assert.equal(gameStatus.player.lives, 2, "Player has 2 lives left");
 assert.equal(gameStatus.isGameWon, false, "Game has not been won");
 assert.equal(gameStatus.activeMines, game.getActiveMines(), "There are mines left");
 tearDown();
+
+console.log("test a board square at start of game");
+setUp();
+square = game.getSquareAt({ row: 0, col: 0});
+assert.equal(square.player, game.getPlayer(), "First square contains the player");
+assert.equal(square.graphic, "[P]", "First square has a player graphic");
+assert.equal(square.hasMine, false, "First square never contains a mine");
+assert.equal(square.hasHit, false, "First square never has a mine so never records a hit");
+tearDown();
+
+game.displayBoard();
 
 
